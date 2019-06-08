e6b3a17bc2032616f2fd60d39a6a3ac6b72e8975
Nikita Zhukov <nikita.zhukov@gmail.com>
2019-06-08 12:56:25 +0300

    add synthetic tests for forEach and reduce

---
+++ forEachAndReduceTest.js
@@ -0,0 +1,35 @@
+const crypto = require('crypto');
+const _ = require('lodash');
+
+const data = Array.from(new Array(100000), () => ({
+  id: crypto.randomBytes(16).toString('hex'),
+  user: _.random(1, 10)
+}));
+
+console.time('forEach');
+
+const result1 = {};
+
+data.forEach(({ id, user }) => {
+  if (!result1[user]) {
+    result1[user] = [];
+  }
+
+  result1[user].push(id);
+});
+
+console.timeEnd('forEach');
+
+console.time('reduce');
+
+const result2 = data.reduce((ac, { id, user }) => {
+  if (!ac[user]) {
+    ac[user] = [];
+  }
+
+  ac[user].push(id);
+
+  return ac;
+}, {});
+
+console.timeEnd('reduce');
