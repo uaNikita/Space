8b117cbab4d98114b83e2d787f2dfdb7e6afa586
Nikita Zhukov <nikita.zhukov@gmail.com>
2019-06-08 13:01:28 +0300

    add more clear output of results

--- forEachAndReduceTest.js
+++ forEachAndReduceTest.js
@@ -1,35 +1,39 @@
 const crypto = require('crypto');
 const _ = require('lodash');
 
-const data = Array.from(new Array(100000), () => ({
+const data = Array.from(new Array(1000000), () => ({
   id: crypto.randomBytes(16).toString('hex'),
   user: _.random(1, 10)
 }));
 
-console.time('forEach');
+_.times(10, () => {
+  console.time('forEach');
 
-const result1 = {};
+  const result1 = {};
 
-data.forEach(({ id, user }) => {
-  if (!result1[user]) {
-    result1[user] = [];
-  }
+  data.forEach(({ id, user }) => {
+    if (!result1[user]) {
+      result1[user] = [];
+    }
 
-  result1[user].push(id);
+    result1[user].push(id);
+  });
+
+  console.timeEnd('forEach');
+
+  console.time('reduce');
+
+  const result2 = data.reduce((ac, { id, user }) => {
+    if (!ac[user]) {
+      ac[user] = [];
+    }
+
+    ac[user].push(id);
+
+    return ac;
+  }, {});
+
+  console.timeEnd('reduce');
+
+  console.log('---------------');
 });
-
-console.timeEnd('forEach');
-
-console.time('reduce');
-
-const result2 = data.reduce((ac, { id, user }) => {
-  if (!ac[user]) {
-    ac[user] = [];
-  }
-
-  ac[user].push(id);
-
-  return ac;
-}, {});
-
-console.timeEnd('reduce');
