43d10334bfcc48698711636d2f5d8cf6f7965d01
Nikita Zhukov <nikita.zhukov@gmail.com>
2022-10-27 18:07:17 +0300

    change formatting in Fibonacci Number.js

--- Fibonacci Number.js
+++ Fibonacci Number.js
@@ -1,13 +1,13 @@
 const cache = {};
 
 const fib = n => {
-	if (cache[n] !== undefined) {
-		return cache[n];
-	}
+  if (cache[n] !== undefined) {
+    return cache[n];
+  }
 
-	const value = n < 2 ? n : fib(n - 2) + fib(n - 1);
+  const value = n < 2 ? n : fib(n - 2) + fib(n - 1);
 
-	cache[n] = value;
+  cache[n] = value;
 
-	return value;
-}
+  return value;
+};
