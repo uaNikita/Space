e501c1a9eec1b7fdeb23a788e4fbc150707ecebd
Nikita Zhukov <nikita.zhukov@gmail.com>
2022-10-27 17:27:23 +0300

    add Fibonacci Number

---
+++ Fibonacci Number.js
@@ -0,0 +1,13 @@
+const cache = {};
+
+const fib = n => {
+	if (cache[n] !== undefined) {
+		return cache[n];
+	}
+
+	const value = n < 2 ? n : fib(n - 2) + fib(n - 1);
+
+	cache[n] = value;
+
+	return value;
+}
